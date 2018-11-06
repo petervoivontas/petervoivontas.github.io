334853a7407047fe9a60038bf8163a3b86365460
Peter Voivontas <petervoivontas@outlook.com>
2018-11-06 22:53:21 +0200

    Updates

---
+++ precache-manifest.f204fa8385ddb0ba9181720bc4810950.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b514fbf346f8363eed03955504c0a93",
+    "url": "/static/media/twitter-icon.4b514fbf.png"
+  },
+  {
+    "revision": "0f7ddc24583501b1de6e5e652c6fb450",
+    "url": "/static/media/searchicon.0f7ddc24.png"
+  },
+  {
+    "revision": "9b224c9884c2bb081d4ac92b23675c2e",
+    "url": "/static/media/google-icon.9b224c98.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82a5062f3a8b4d20e937",
+    "url": "/static/js/main.82a5062f.chunk.js"
+  },
+  {
+    "revision": "3777e0bf210127a21ce6",
+    "url": "/static/js/1.3777e0bf.chunk.js"
+  },
+  {
+    "revision": "82a5062f3a8b4d20e937",
+    "url": "/static/css/main.97ab8c00.chunk.css"
+  },
+  {
+    "revision": "c5977da5a7874a60e4e4bfc4e6f9c59d",
+    "url": "/index.html"
+  }
+];
